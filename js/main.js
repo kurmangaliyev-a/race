454c8c5aa92d0c00e00b57f47a5dfc07bbc2e737
KurmangaliyevAzamat <kurmangaliev.azamat1996@gmail.com>
2020-09-11 00:03:26 +0600

    key functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-alert("hello");
+const score = document.querySelector('.score'),
+    start=document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea');
+
+
+start.addEventListener('click', startGame);
+
+function startGame(){
+    start.classList.add('hide');
+}
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+function startRun(){
+    console.log('start');
+}
+
+function stopRun() {
+    console.log('stop');
+}
